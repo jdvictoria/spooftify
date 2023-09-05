abdd2a989bf89feef84f2dbda3527bf631819389
jdvictoria <jdvictoria.tech@gmail.com>
2023-09-05 21:38:38 +0800

    Implement Body and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,60 @@ import styled from "styled-components";
 import {Container} from "@mui/joy";
 
 const AppContainer = styled(Container)`
-  display: flex;
-  flex-direction: column;
-  
-  justify-content: center; // vertical
-  align-items: center; // horizontal
-  
-  max-width: 100%;
+  overflow-y: hidden;
+  width: 100vw;
   height: 100vh;
+`;
+
+export const Body = styled(Container)`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  
+  width: 100%;
+  height: 80%;
+  
+  justify-content: center;
+  align-items: center;
   
   background-color: red;
 `;
 
+export const Nav = styled(Container)`
+  display: flex;
+  position: relative;
+  
+  width: 100vw;
+  height: 80vh;
+  
+  justify-content: center;
+  align-items: center;
+  
+  background-color: red;
+`;
+
+export const Footer = styled(Container)`
+  display: flex;
+  position: relative;
+
+  width: 100%;
+  height: 20%;
+  
+  justify-content: center;
+  align-items: center;
+  
+  background-color: orange;
+`;
+
 function App() {
   return (
-    <AppContainer maxWidth={false}>
+    <AppContainer maxWidth={false} disableGutters>
+      <Body maxWidth={false}>
+
+      </Body>
+      <Footer maxWidth={false}>
+
+      </Footer>
     </AppContainer>
   );
 }
